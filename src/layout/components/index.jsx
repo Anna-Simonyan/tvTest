987d0f3deb7dd4b28926f5d7936868131f52b687
Agnessa Antabyan <agnessaantabyan6@gmail.com>
2023-01-05 18:36:29 +0400

    fetch new data from server

--- src/layout/components/index.jsx
+++ src/layout/components/index.jsx
@@ -6,9 +6,10 @@ import { selectFilms } from "store/selector";
 
 
 
-const Header = () => {
+const Header = ({filmItem}) => {
   const { pathname, state } = useLocation();
   const { filmItems } = useSelector(selectFilms);
+  
   const { id } = useParams();
 
   return (
@@ -21,8 +22,8 @@ const Header = () => {
           <>
             <div className="header__home-texts">
               <p className="header__home-text1">TV Show and web series database.</p>
-              <p className="header__home-text2">Create personalised schedules.</p>
-              <p className="header__home-text3">Episode guide,cast,crew and character information.</p>
+              <p className="header__home-text2">Create personalised schedules. Episode guide,cast,crew and</p>
+              <p className="header__home-text3"> character information.</p>
             </div>
           </>
         ) : (
@@ -34,12 +35,12 @@ const Header = () => {
             </div>
             <div className="header__rigth">
               
-            <div> <Rating name="size-small" defaultValue={state?.rating} max={5} /></div> 
+            <div className="header__rigth-inner"> <Rating name="size-small" defaultValue={state?.rating} max={5} /> <div>{filmItems?.rating?.average}</div> </div> 
              <div>{filmItems?.show?.rating?.average}</div>
               <h3 className="header__rigth-h3">{filmItems?.name}</h3>
-              <p className="header__rigth-text">{filmItems.summary}</p>
+              <p className="header__rigth-text">{filmItems.summary }</p>
             </div>
-          </div>
+          </div>           
         )}
       </div>
     </div>
